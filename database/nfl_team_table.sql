948c41a7b2aeffb80e202ae1ecb9069a5bab0720
michaelkot1 <kotmichael7@gmail.com>
2026-02-16 22:50:47 -0800

    script for the postgresql table nfl_team.

---
+++ database/nfl_team_table.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE nfl_team (
+--     team_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     team_name VARCHAR(50),
+--     team_abbr VARCHAR(25),
+--     team_conf VARCHAR(50),
+--     team_division VARCHAR(25)
+-- );
+
+
+SELECT * FROM nfl_team;
+
+-- DROP TABLE IF EXISTS nfl_team;
